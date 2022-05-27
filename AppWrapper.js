bc4396b542ef0b409e9852611a85f8a0b454d18f
imhilla <hillaryodhiambo282@gmail.com>
2022-05-27 09:05:01 +0300

    create store

---
+++ AppWrapper.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function AppWrapper() {
+  return (
+    <View>
+      <Text>AppWrapper</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
